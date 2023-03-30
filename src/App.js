17ac454ed4bd4eeeb5cd892a8af6ee3cd0e5a952
David Jordan <wordnerd92@gmail.com>
2023-03-29 22:50:57 -0500

    added routes and pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Home from "./Pages/Home";
+import Projects from "./Pages/Projects";
+import Experience from "./Pages/Experience";
 
 function App() {
   return (
     <div className="App">
-      <h1> My Portfolio </h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/experience" element={<Experience />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
